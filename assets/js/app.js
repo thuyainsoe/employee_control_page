631e3903ae766bddb5820cb1d1e98634236541da
ThuYainSoe <61083196+thuyainsoe@users.noreply.github.com>
2022-09-25 12:18:59 +0630

    Final Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,8 @@ let mainCheck = document.querySelector("#mainCheck")
 let check = document.querySelector("#check");
 let checkAll = document.querySelectorAll("#check");
 let checks = document.querySelectorAll("#check");
-let empCard = document.querySelectorAll('.employee_card');
+let editIcon = document.querySelector(".employee_icon_3_container");
+
 checks.forEach(check => {
     check.addEventListener('change', (e)=> {
         let arr = [];
@@ -12,9 +13,41 @@ checks.forEach(check => {
         }
         if(arr.includes(true)){
             mainCheck.checked = true;
+            editIcon.style.display = "block";
         }
         else{
             mainCheck.checked = false;
+            editIcon.style.display = "none";
+        }
+    });
+});
+
+// Main Check
+mainCheck.addEventListener('change',()=>{
+    if(!mainCheck.checked){
+        for(let i = 0; i < checks.length ; i++){
+            checks[i].checked = false;
+            editIcon.style.display = "none";
+            checks[i].parentElement.parentElement.style.borderLeft = "none";
+        }
+    }
+    else{
+        for(let i = 0; i < checks.length ; i++){
+            checks[i].checked = true;
+            editIcon.style.display = "block";
+            checks[i].parentElement.parentElement.style.borderLeft = "3px solid #1ED292";
+        }
+    }
+});
+
+//For Border Left
+checks.forEach(check => {
+    check.addEventListener('change',()=>{
+        if(check.checked){
+            check.parentElement.parentElement.style.borderLeft = "3px solid #1ED292";
+        }
+        else{
+            check.parentElement.parentElement.style.borderLeft = "none";
         }
     });
 });
@@ -22,4 +55,3 @@ checks.forEach(check => {
 
 
 
-//For Border Left
